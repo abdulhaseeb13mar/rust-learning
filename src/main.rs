3f348a8b9932fb9d14e990f93db67f716ed51717
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2022-07-01 20:08:16 +0500

    Fahrenheit to celsius

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use std::io::stdin;
+
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+    let mut f_temp = String::new();
+    println!("Enter a temperature in Fahrenheit: ");
+    stdin().read_line(&mut f_temp).expect("failed to read line");
+    let f_temp: f32 = match f_temp.trim().parse() {
+        Ok(num) => num,
+        Err(_) => panic!("Invalid input"),
+    };
+    let f_temp = fahrenheit_to_celsius(f_temp);
+    println!("{} degrees celsius", f_temp);
+}
+
+fn fahrenheit_to_celsius(temp: f32) -> f32 {
+    let celsius = (temp - 32.0) * 5.0 / 9.0;
+    celsius
 }
